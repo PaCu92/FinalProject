194784b426e1cbb35ec93725532fb12ba113ce5a
IvanDemetrio <ivandemetriovargas@gmail.com>
2019-06-21 08:59:48 +0200

    x

--- ProgettoFinale/client/src/app/components/carello/carello.component.ts
+++ ProgettoFinale/client/src/app/components/carello/carello.component.ts
@@ -7,17 +7,10 @@ import { Articoli } from 'src/app/model/articoli';
   styleUrls: ['./carello.component.css']
 })
 export class CarelloComponent implements OnInit {
-<<<<<<< HEAD
 @Input()cart;
   constructor() { }
 
   ngOnInit() { 
-=======
-@Input() cart;
-  constructor() { console.log(this.cart);}
-
-  ngOnInit() { console.log(this.cart)
->>>>>>> d098fd5849006a15c62afeaa76281d9ea2890be1
   }
 
 }
